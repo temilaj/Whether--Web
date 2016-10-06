bdfcbb303d844c85f704076e18461de5d88a277d
Temi Lajumoke <temilaj@hotmail.com>
2016-10-06 07:35:00 +0100

    weathericon font integration

--- src/js/whether.js
+++ src/js/whether.js
@@ -13,17 +13,132 @@ $('document').ready(function () {
 });
 
 function loadWeather(location, woeid) {
+    //$weathercode = weather.code
     $.simpleWeather({
         location: location,
         woeid: woeid,
         unit: 'f',
         success: function (weather) {
-            html = '<div class="weather-icon icon-' + weather.code + '"></div><h1>' + weather.alt.temp + '&deg;C' + '</h1><p>' + weather.city + weather.region + '</p><span>' + weather.currently + '</span>';
+            html = '<i class="weather-icon wi ' + setWeatherIcon(weather.code) + '"></i>' + '<h1>' + weather.alt.temp + '&deg;C' + '</h1><p>' + weather.city + weather.region + '</p><span>' + weather.currently + '</span>';
 
             $("#weather").html(html);
+
+
         },
         error: function (error) {
             $("#weather").html('<p>' + error + '</p>');
         }
     });
+    //$myweather = setWeatherIcon(weather.code);
+
+    function setWeatherIcon(condid) {
+        var icon = '';
+        var weather = '';
+        switch (condid) {
+            case 0: icon = 'wi-tornado';
+                break;
+            case '1': icon = 'wi-storm-showers';
+                break;
+            case '2': icon = 'wi-tornado';
+                break;
+            case '3': icon = 'wi-thunderstorm weather-rainy';
+                break;
+            case '4': icon = 'wi-thunderstorm rainy';
+                break;
+            case '5': icon = 'wi-snow';
+                break;
+            case '6': icon = 'wi-rain-mix';
+                break;
+            case '7': icon = 'wi-rain-mix';
+                break;
+            case '8': icon = 'wi-sprinkle';
+                break;
+            case '9': icon = 'wi-sprinkle';
+                break;
+            case '10': icon = 'wi-hail';
+                break;
+            case '11': icon = 'wi-showers';
+                break;
+            case '12': icon = 'wi-showers';
+                break;
+            case '13': icon = 'wi-snow';
+                break;
+            case '14': icon = 'wi-storm-showers';
+                break;
+            case '15': icon = 'wi-snow';
+                break;
+            case '16': icon = 'wi-snow';
+                break;
+            case '17': icon = 'wi-hail';
+                break;
+            case '18': icon = 'wi-hail';
+                break;
+            case '19': icon = 'wi-cloudy-gusts';
+                break;
+            case '20': icon = 'wi-fog';
+                break;
+            case '21': icon = 'wi-fog';
+                break;
+            case '22': icon = 'wi-fog';
+                break;
+            case '23': icon = 'wi-cloudy-gusts';
+                break;
+            case '24': icon = 'wi-cloudy-windy';
+                break;
+            case '25': icon = 'wi-thermometer';
+                break;
+            case '26': icon = 'wi-cloudy';
+                break;
+            case '27': icon = 'wi-night-cloudy';
+                break;
+            case '28': icon = 'wi-day-cloudy';
+                break;
+            case '29': icon = 'wi-night-cloudy';
+                break;
+            case '30': icon = 'wi-day-cloudy';
+                break;
+            case '31': icon = 'wi-night-clear';
+                break;
+            case '32': icon = 'wi-day-sunny';
+                break;
+            case '33': icon = 'wi-night-clear';
+                break;
+            case '34': icon = 'wi-day-sunny-overcast';
+                break;
+            case '35': icon = 'wi-hail';
+                break;
+            case '36': icon = 'wi-day-sunny';
+                break;
+            case '37': icon = 'wi-thunderstorm';
+                break;
+            case '38': icon = 'wi-thunderstorm';
+                break;
+            case '39': icon = 'wi-thunderstorm';
+                break;
+            case '40': icon = 'wi-storm-showers';
+                break;
+            case '41': icon = 'wi-snow';
+                break;
+            case '42': icon = 'wi-snow';
+                break;
+            case '43': icon = 'wi-snow';
+                break;
+            case '44': icon = 'wi-cloudy';
+                break;
+            case '45': icon = 'wi-lightning';
+                break;
+            case '46': icon = 'wi-snow';
+                break;
+            case '47': icon = 'wi-thunderstorm';
+                break;
+            case '3200': icon = 'wi-cloud';
+                break;
+            default: icon = 'wi-cloud';
+                break;
+        }
+
+        //weather = '<i class="' + 'wi ' + icon + '"';
+        return icon;
+
+    }
 }
